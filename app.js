2cd3b45c6f53a953b99369a7b58813ed4463b3de
surya <jyothikasurya9966@gmail.com>
2021-09-28 19:41:17 +0530

    js basics data types and operators

--- app.js
+++ app.js
@@ -6,3 +6,49 @@ console.log(typeof 1.32434363424)
 console.log(typeof true)
 var age=null
 console.log(age)
+var arr=[1,"surya",true]
+console.log(arr)
+
+console.log(arr.length)
+arr.push(5)
+console.log(arr)
+var a=19
+a=22
+console.log(a)
+b=11
+console.log(b-a)
+//assignment ops
+b=11
+console.log('b',b)
+b+=1
+console.log('b+=1',b)
+b++
+console.log('b++',b)
+b-=10
+console.log('b-=10',b)
+b--
+console.log('b--',b)
+
+var c=10
+console.warn(c++)
+console.warn(c)
+console.warn(++c)
+//comparission ops
+var w=22
+var e=34
+var t='22'
+console.log(w<=e)
+console.log(w==e)
+console.log(e>=w)
+console.warn(w==t)
+console.log(w===t)
+//logical ops
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
